6b604bea228b25730c55dc0db3dce486b71129c5
Bito <tyrion72@gmail.com>
2023-10-03 16:50:40 +0200

    Initial Commit

---
+++ iTourSwiftData/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iTourSwiftData
+//
+//  Created by Alberto Alegre Bravo on 3/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iTourSwiftData/iTourSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourSwiftDataApp.swift
+//  iTourSwiftData
+//
+//  Created by Alberto Alegre Bravo on 3/10/23.
+//
+
+import SwiftUI
+
+@main
+struct iTourSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
